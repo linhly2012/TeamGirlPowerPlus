64ee406b46322b3b94c015f087e630559866d4a1
Linh <linh199669@gmail.com>
2017-05-30 01:00:28 -0700

    changed the data for year of 2013. For the year of 2013, we will be using 2012 -2013 school year, due the weird way of school year being calculated. [IMPORTANT NOTE: Idaho does not has any information for 2012-13]

--- educationInfo.R
+++ educationInfo.R
@@ -2,14 +2,19 @@ library('dplyr')
 library('readxl')
 
 #set working directory
-setwd("/Users/linhly/Desktop/INFO/TeamGirlPowerPlusOne")
+#setwd("/Users/linhly/Desktop/INFO/TeamGirlPowerPlusOne")
 
 #value is go by percent of students 
 #the data has been clean in a readable format through Google Drive excel 
 #all the value in the data infomration go by "PERCENT OF STUDENT" for each 
 #variable
-#reading the graduation rate of HS students in each state 
-grad_rates_df <- read.csv('./data/education-grad-rate-revised-data.csv')
+#reading the graduation rate of HS students in each state
+
+#for the year of 2014 - data will be using is 2013 - 2014
+grad_rates_13_14 <- read.csv('./data/education-grad-rate-revised-data-2013-2014.csv')
+
+#for the year of 2013 - data will be using is 2012 - 2013
+grad_rates_12_13 <- read.csv('./data/education-grad-rates-revised-2012-2013.csv')
 
 #reading in crimerates data 
 crime_rates <- read.csv('./data/CrimeStateByState_Clean.csv')
@@ -21,7 +26,13 @@ dir.create('States Level Data')
 #looking into to two df, due to two differen years 2013 and 2014
 FilterDfYear <- function (year) {
   df_state_crime_year <- crime_rates %>% filter(Year == year)
-  df <- merge(x=df_state_crime_year, y=grad_rates_df, by='State')
+  #df <- merge(x=df_state_crime_year, y=grad_rates_df, by='State')
+  if(year == 2013) {
+    df <- merge(x=df_state_crime_year, y=grad_rates_12_13, by='State')
+  }
+  else {
+    df <- merge(x=df_state_crime_year, y=grad_rates_13_14, by='State')    
+  }
   
   path <- paste0('./States Level Data/Crime And Education Rate', year, '-StatesLevel.csv')
   #to csv file for each year 
@@ -29,6 +40,13 @@ FilterDfYear <- function (year) {
   return(df)
 }
 
+#there will be a column name TOTAL -> it is the value of 
+#PERCENT OF STUDENTS gradute from that state during that
+#school year.
+
+#note for JoinedCrimeAndEd2013 df, there will be only 50 obs.
+#because in 2012-2013 school year, Idoho about graduation rate 
+#data is not available.
 JoinedCrimeAndEd2013 <- FilterDfYear(2013)
 JoinedCrimeAndEd2014 <- FilterDfYear(2014)
 
