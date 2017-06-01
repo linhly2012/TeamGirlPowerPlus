0da08da847228119775f6bf106976f8fc4bbaa09
shjang1025 <shjang@uw.eduexit>
2017-06-01 00:35:37 -0700

    change bar plot to scatter plot

--- testing ui and server/server.R
+++ testing ui and server/server.R
@@ -18,30 +18,45 @@ shinyServer(function(input, output) {
     color <- input$color
     
     if(input$year == '2013') {
-      x <- CAER2013$State
-      y1 <- CAER2013[,input$crime]
-      y2 <- CAER2013[,input$`student type`]
-      data <- data.frame(x, y1, y2)
+      #x <- CAER2013$State
+      Crime.Rate <- CAER2013[,input$crime]
+      Education.Rate <- CAER2013[,input$`student type`]
+      data <- data.frame(Crime.Rate, Education.Rate)
       
-      data$x <- factor(data$x, levels = data[["x"]])
-
-      plot_ly(data, x = ~x, y = ~y1, type = 'bar', name = 'Crime rate', marker = list(color = color, line = list(color = color))) %>%
-        add_trace(y = ~y2, name = 'Education rate', marker = list(color = 'rgb(70,130,180)')) %>%
-        layout(title = paste0("US Crime and Education rate in", input$year), xaxis = list(title = "", tickangle = -45),yaxis = list(title = ""),
-               margin = list(b = 100),
-               barmode = 'group')
+      #data$x <- factor(data$x, levels = data[["x"]])
+      
+      plot_ly(data, x = ~Crime.Rate, y = ~Education.Rate, marker = list(size = 8,color = color,
+              line = list(color = color, width = 2)), text = ~paste('Crime Rate', Crime.Rate,'Education Rate', Education.Rate, sep = "<br />") ) %>%
+              layout(title = 'Correlation of Crime rate and Education rate',
+                    yaxis = list(zeroline = FALSE),
+                    xaxis = list(zeroline = FALSE))
+     
+      
+      #plot_ly(data, x = ~x, y = ~y1, type = 'bar', name = 'Crime rate', marker = list(color = color, line = list(color = color))) %>%
+        #add_trace(y = ~y2, name = 'Education rate', marker = list(color = 'rgb(70,130,180)')) %>%
+      #  layout(title = paste0("US Crime and Education rate in", input$year), xaxis = list(title = "", tickangle = -45),yaxis = list(title = ""),
+      #         margin = list(b = 100),
+      #         barmode = 'group')
+      
       
     } else {
-      x <- CAER2014$State
-      y1 <- CAER2014[,input$crime]
-      y2 <- CAER2014[,input$`student type`]
-      data <- data.frame(x, y1, y2)
-      data$x <- factor(data$x, levels = data[["x"]])
-      plot_ly(data, x = ~x, y = ~y1, type = 'bar', name = 'Crime rate', marker = list(color = color, line = list(color = color))) %>%
-        add_trace(y = ~y2, name = 'Education rate', marker = list(color = 'rgb(70,130,180)')) %>%
-        layout(title = paste0("US Crime and Education rate in", input$year), xaxis = list(title = "", tickangle = -45),yaxis = list(title = ""),
-               margin = list(b = 100),
-               barmode = 'group')
+      #x <- CAER2014$State
+      Crime.Rate <- CAER2014[,input$crime]
+      Education.Rate <- CAER2014[,input$`student type`]
+      data <- data.frame(Crime.Rate, Education.Rate)
+      #data$x <- factor(data$x, levels = data[["x"]])
+      
+      plot_ly(data, x = ~Crime.Rate, y = ~Education.Rate,marker = list(size = 8,color = color,
+                                                                       line = list(color = color, width = 2))) %>%
+        layout(title = 'Correlation of Crime rate and Education rate',
+               yaxis = list(zeroline = FALSE),
+               xaxis = list(zeroline = FALSE))
+      
+      #plot_ly(data, x = ~x, y = ~y1, type = 'bar', name = 'Crime rate', marker = list(color = color, line = list(color = color))) %>%
+        #add_trace(y = ~y2, name = 'Education rate', marker = list(color = 'rgb(70,130,180)')) %>%
+      #  layout(title = paste0("US Crime and Education rate in", input$year), xaxis = list(title = "", tickangle = -45),yaxis = list(title = ""),
+      #         margin = list(b = 100),
+      #         barmode = 'group')
     }
 
   })
